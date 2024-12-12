77c0a88b1265dbc0e16f48fa8a8465bc7ef861a2
Manix <victor.moncada001@gmail.com>
2024-12-12 13:04:30 -0300

    medificacion de archivo test_auth.py

--- prueba/test_auth.py
+++ prueba/test_auth.py
@@ -1,13 +1,12 @@
 from autenticacion.auth import registrar_usuario, iniciar_sesion
 
 def test_registro():
-    registrar_usuario("usuario_prueba", "12345")
+    registrar_usuario("admin", "12345")
     print("Test de registro exitoso.")
 
 def test_login():
-    assert iniciar_sesion("usuario_prueba", "12345") == True
+    assert iniciar_sesion("admin", "12345") == True
     print("Test de inicio de sesión exitoso.")
 
-if _name_ == "_main_":
-    test_registro()
-    test_login()
+test_registro()
+test_login()
